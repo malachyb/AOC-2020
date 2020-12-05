85ae37f62eb41966d53cf30538db4b02a8a63d81
malmal200 <malachybyrne1@gmail.com>
2020-12-05 17:20:32 +0000

    Day five task one refactored

--- day-5-task-1.py
+++ day-5-task-1.py
@@ -2,11 +2,7 @@ from sys import stdin
 
 
 def seat_id(boarding_pass: str):
-    row_id = boarding_pass[:7]
-    column_id = boarding_pass[7:]
-    row = int(row_id.replace('F', '0').replace('B', '1'), 2)
-    column = int(column_id.replace('L', '0').replace('R', '1'), 2)
-    return row * 8 + column
+    return int(boarding_pass.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2)
 
 
 def main():
